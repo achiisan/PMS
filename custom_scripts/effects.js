9a711adf41463aa563578ff9d7e658f8d86a73d2
achiisan@github.com <mddoron@up.edu.ph>
2014-11-05 17:05:28 +0800

    Added initial config files (Bootstrap/Home Page)

---
+++ custom_scripts/effects.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('#main-title').hide(2);
+	$('#buttonplan').hide(2);
+	$('#main_content_slab').hide(2);
+	$('#main-title').show(1000);
+	
+	$('#main_content_slab').show(5000);
+	$('#buttonplan').show(6000);
+
+
+});
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<script src="custom_scripts/jquery-2.1.1.js"></script>
+		<link type="text/css" rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
+		<link type="text/css" rel="stylesheet" href="custom_scripts/styler.css"/>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+		<script src="custom_scripts/effects.js"></script>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<div id="main-wrapper">
+			<div id="main_content_slab" class="col-lg-4 col-xs-12 col-md-12">
+				<h1>Home <span class="glyphicon glyphicon-home"></span></h1>
+				<h1>My Parties <span class="glyphicon glyphicon-pushpin"></span></h1>
+				<h1>Contact Us! <span class="glyphicon glyphicon-phone"></span></h1>
+			</div>
+
+			<div id="main_content" class="col-xs-12 col-lg-8 col-md-8">
+				<div id="main-title">
+					<img src="img/title.png"/>
+				</div>
+				<button class="btn btn-lg btn-warning" id="buttonplan">Plan my party now!</button>
+
+
+			</div>
+
+			<div id="footer_design" class="col-xs-12 col-lg-12 col-md-12">
+				
+					<img src="img/pizza.png"/>
+			
+			</div>
+		</div>
+	</body>
+</html>
